7882aec6434c4bd9a78034a4cd769b9ae2a2442f
FadillaRatnaDwita29RPL <fadilladwita20@gmail.com>
2022-04-10 04:42:08 +0700

    add service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 
@@ -14,7 +14,7 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
+serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
